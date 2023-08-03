bde2121564e504582dc429c72a004c0766963af5
Erkan Bostan <erkanbstn34@gmail.com>
2023-08-03 19:34:15 +0300

    AutoMapper and Lazy Object Mapper Installed, Created.

---
+++ AuthServer.Core.Service/DtoMapper.cs
@@ -0,0 +1,15 @@
+﻿using AuthServer.Core.Core.Dtos;
+using AuthServer.Core.Core.Model;
+using AutoMapper;
+
+namespace AuthServer.Core.Service
+{
+    public class DtoMapper : Profile
+    {
+        public DtoMapper()
+        {
+            CreateMap<ProductDto, Product>().ReverseMap();
+            CreateMap<UserAppDto, UserApp>().ReverseMap();
+        }
+    }
+}
---
+++ AuthServer.Core.Service/ObjectMapper.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+
+namespace AuthServer.Core.Service
+{
+    public class ObjectMapper
+    {
+        private static readonly Lazy<IMapper> _mapper = new Lazy<IMapper>(() =>
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<DtoMapper>();
+            });
+            return config.CreateMapper();
+        });
+        public static IMapper Mapper => _mapper.Value;
+    }
+}
